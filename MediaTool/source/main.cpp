3fca94ee33014f74d6ed304cc24a98213342849f
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-05-23 20:01:32 +0200

    add argv input

--- MediaTool/source/main.cpp
+++ MediaTool/source/main.cpp
@@ -4,6 +4,8 @@
 int wmain( int argc, wchar_t** argv )
 {
     mt::MediaTool media_tool{};
+    if ( argc > 1 )
+        media_tool.ffmpeg_section->input_file = argv[1];
     while ( media_tool.update() );
     return 0;
 }
