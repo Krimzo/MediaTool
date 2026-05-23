d09f3dc8b08530553926901705dfb75e456fd2ba
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-05-23 15:33:35 +0200

    improve system section

--- MediaTool/source/system_section.cpp
+++ MediaTool/source/system_section.cpp
@@ -1,16 +1,60 @@
 #include "system_section.h"
+#include "ytdlp_section.h"
+#include "ffmpeg_section.h"
 
 const kl::Float4 mt::SystemSection::COLOR = kl::RGB{ 160, 236, 255 };
 
 void mt::SystemSection::display()
 {
-    im::PushStyleColor( ImGuiCol_Text, (ImU32) ImColor( 235, 99, 99 ) );
-    if ( im::Button( QNAME( "Reinstall YT-DLP" ) ) )
-        execute( LR"(winget install "yt-dlp" --skip-dependencies)" );
+    static const kl::Float4 PYTHON_COLOR = kl::RGB{ 178, 255, 178 };
+    static const kl::Float4 DENO_COLOR = kl::RGB{ 125, 240, 255 };
 
-    im::PushStyleColor( ImGuiCol_Text, (ImU32) ImColor( 155, 227, 104 ) );
-    if ( im::Button( QNAME( "Reinstall FFMPEG" ) ) )
-        execute( LR"(winget install "ffmpeg" --skip-dependencies)" );
+    static constexpr std::string_view PYTHON_INSTALL_TEXT = "Install/Upgrade PYTHON (yt-dlp requirement)";
+    static constexpr std::string_view DENO_INSTALL_TEXT = "Install/Upgrade DENO (yt-dlp requirement)";
+    static constexpr std::string_view YTDLP_INSTALL_TEXT = "Install/Upgrade YT-DLP";
+    static constexpr std::string_view FFMPEG_INSTALL_TEXT = "Install/Upgrade FFMPEG";
 
-    im::PopStyleColor( 2 );
+    static constexpr std::string_view PYTHON_INSTALL_COMMAND = R"(winget install python)";
+    static constexpr std::string_view DENO_INSTALL_COMMAND = R"(winget install deno)";
+    static constexpr std::string_view YTDLP_INSTALL_COMMAND = R"(pip install yt-dlp)";
+    static constexpr std::string_view FFMPEG_INSTALL_COMMAND = R"(winget install ffmpeg)";
+
+    const ImVec2 DEFAULT_BUTTON_SIZE = { im::GetContentRegionAvail().x * .6f, 25.0f };
+
+    im::PushStyleVar( ImGuiStyleVar_FramePadding, { 5, 5 } );
+
+    im::PushStyleColor( ImGuiCol_Text, (ImU32) ( ImColor ) reinterpret_cast<ImVec4 const&>( PYTHON_COLOR ) );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - im::CalcTextSize( PYTHON_INSTALL_TEXT.data() ).x * .5f );
+    im::SetCursorPosY( im::GetCursorPosY() + 25.0f );
+    im::Text( PYTHON_INSTALL_TEXT.data() );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - DEFAULT_BUTTON_SIZE.x * .5f );
+    if ( im::Button( QNAME( PYTHON_INSTALL_COMMAND.data() ), DEFAULT_BUTTON_SIZE ) )
+        execute( kl::convert_string( PYTHON_INSTALL_COMMAND ) );
+
+    im::PushStyleColor( ImGuiCol_Text, (ImU32) ( ImColor ) reinterpret_cast<ImVec4 const&>( DENO_COLOR ) );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - im::CalcTextSize( DENO_INSTALL_TEXT.data() ).x * .5f );
+    im::SetCursorPosY( im::GetCursorPosY() + 25.0f );
+    im::Text( DENO_INSTALL_TEXT.data() );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - DEFAULT_BUTTON_SIZE.x * .5f );
+    if ( im::Button( QNAME( DENO_INSTALL_COMMAND ), DEFAULT_BUTTON_SIZE ) )
+        execute( kl::convert_string( DENO_INSTALL_COMMAND ) );
+
+    im::PushStyleColor( ImGuiCol_Text, (ImU32) ( ImColor ) reinterpret_cast<ImVec4 const&>( YTDLPSection::COLOR ) );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - im::CalcTextSize( YTDLP_INSTALL_TEXT.data() ).x * .5f );
+    im::SetCursorPosY( im::GetCursorPosY() + 25.0f );
+    im::Text( YTDLP_INSTALL_TEXT.data() );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - DEFAULT_BUTTON_SIZE.x * .5f );
+    if ( im::Button( QNAME( YTDLP_INSTALL_COMMAND ), DEFAULT_BUTTON_SIZE ) )
+        execute( kl::convert_string( YTDLP_INSTALL_COMMAND ) );
+
+    im::PushStyleColor( ImGuiCol_Text, (ImU32) ( ImColor ) reinterpret_cast<ImVec4 const&>( FFMPEGSection::COLOR ) );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - im::CalcTextSize( FFMPEG_INSTALL_TEXT.data() ).x * .5f );
+    im::SetCursorPosY( im::GetCursorPosY() + 25.0f );
+    im::Text( FFMPEG_INSTALL_TEXT.data() );
+    im::SetCursorPosX( im::GetContentRegionAvail().x * .5f - DEFAULT_BUTTON_SIZE.x * .5f );
+    if ( im::Button( QNAME( FFMPEG_INSTALL_COMMAND ), DEFAULT_BUTTON_SIZE ) )
+        execute( kl::convert_string( FFMPEG_INSTALL_COMMAND ) );
+
+    im::PopStyleColor( 4 );
+    im::PopStyleVar( 1 );
 }
