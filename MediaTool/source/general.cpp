9894e9f048b896217241abed291bf187ff14bb8e
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-08-06 19:36:25 +0200

    Update general.cpp

--- MediaTool/source/general.cpp
+++ MediaTool/source/general.cpp
@@ -63,7 +63,7 @@ void mt::auto_adjust_window_height( kl::Window const& window )
     if ( window.maximized() )
         return;
 
-    const int final_cursor_y = kl::min( (int) ImGui::GetCursorScreenPos().y, int( kl::SCREEN_SIZE.x * MAX_SCREEN_HEIGHT_PERC ) );
+    const int final_cursor_y = kl::min( (int) ImGui::GetCursorScreenPos().y, int( kl::SCREEN_SIZE.y * MAX_SCREEN_HEIGHT_PERC ) );
     if ( final_cursor_y != window.height() )
         QUEUED_WINDOW_HEIGHT.emplace( final_cursor_y );
 }
